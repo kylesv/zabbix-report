7727bfd16fb3b7618aa0d36cbfb216c6cb7a2c7e
kylesv <kile-jew@rambler.ru>
2015-05-18 11:43:50 +0300

    file for mail

--- report_mail.php
+++ report_mail.php
@@ -15,6 +15,31 @@
 **
 **/
 
+require_once dirname(__FILE__).'/include/config.inc.php';
+require_once dirname(__FILE__).'/include/report-ts.inc.php';
+require_once dirname(__FILE__).'/include/func.inc.php';
+
+$fields = array(
+	'groupid' =>			array(T_ZBX_INT, O_OPT, P_SYS,	DB_ID,		null),
+	'hostid' =>				array(T_ZBX_INT, O_OPT, P_SYS,	DB_ID,		null),
+	'hostgroupid' =>		array(T_ZBX_INT, O_OPT, P_SYS,	DB_ID,		null),
+	'tpl_triggerid' =>		array(T_ZBX_INT, O_OPT, P_SYS,	DB_ID,		null),
+	'fullscreen' =>			array(T_ZBX_INT, O_OPT, P_SYS,	IN('0,1'),	null),
+	'btnSelect' =>			array(T_ZBX_STR, O_OPT, null,	null,		null),
+	'file'=>				array(T_ZBX_STR, O_OPT, null,	null,		null),
+	'orient'=>				array(T_ZBX_STR, O_OPT, null,	null,		null),
+	// filter
+	'period'=>				array(T_ZBX_INT, O_OPT, null,	null,		null),
+	'elementid'=>				array(T_ZBX_INT, O_OPT, null,	null,		null),
+	'stime'=>				array(T_ZBX_STR, O_OPT, null,	null,		null),
+	'filter_timesince'=>				array(T_ZBX_STR, O_OPT, null,	null,		null),
+	'filter_timetill'=>				array(T_ZBX_STR, O_OPT, null,	null,		null),
+	'action'=>				array(T_ZBX_STR, O_OPT, null,	null,		null)
+	// sort and sortorder
+);
+check_fields($fields);
+
+$config = select_config();
 
 
 
